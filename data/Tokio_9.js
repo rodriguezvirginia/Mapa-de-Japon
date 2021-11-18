a3b511aad92e258b9eb54cf061354ad23018eb7f
rodriguezvirginia <94645829+rodriguezvirginia@users.noreply.github.com>
2021-11-18 17:19:42 -0300

    Add files via upload

---
+++ data/Tokio_9.js
@@ -0,0 +1,8 @@
+var json_Tokio_9 = {
+"type": "FeatureCollection",
+"name": "Tokio_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Chiyoda", "t_pob": "66758", "aÃ±o_fund": "11 de Febrero de 660 a.C", "Idioma": "Japones", "moneda": "yen" }, "geometry": { "type": "Point", "coordinates": [ 139.768299292603814, 35.68072069970362 ] } }
+]
+}
